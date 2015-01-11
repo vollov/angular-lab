4b8003355317c9793172d5844f94d8c75bc591fa
dustin.zhang <vollov.ca@gmail.com>
2015-01-11 17:26:35 -0500

    add sql-server lab code

---
+++ sql-server/data/data.sql
@@ -0,0 +1,6 @@
+USE blog;
+
+INSERT INTO article(title, content) VALUES
+('Hydro','flushes whatever remaining changes remain to the database, and commits the transaction.'), 
+('Gas','inserts new rows in the database, all newly generated identifiers and database-generated defaults become available on the instance, either immediately or via load-on-first-access. In this case, the entire row was re-loaded on access because a new transaction was begun after we issued commit().'), 
+('Water', 'article 3'),('Internet', 'article 4'),('Cable', 'article 5');
